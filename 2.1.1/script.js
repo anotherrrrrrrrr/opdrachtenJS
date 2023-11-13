4dbdc6d1da8002e884a6491fa35e07c5b9299193
another <144031759+anotherrrrrrrrr@users.noreply.github.com>
2023-11-13 13:13:58 +0100

    Add files via upload

---
+++ 2.1.1/script.js
@@ -0,0 +1,8 @@
+var modal = document.getElementById("modal")
+var accept = document.getElementById("accept")
+
+modal.style.display = "block";
+
+accept.onclick = function() {
+  modal.style.display = "none"
+}
